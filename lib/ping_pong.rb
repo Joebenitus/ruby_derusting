99b280b2de1b9d9574bb01c1ee774ffa2f4df5cc
Joseph Karnafel <josephkarnafel@gmail.com>
2021-02-20 11:44:33 -0800

    add passing test for ping_pong funtion replacing all numbers divisible by 5 with pong

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -4,6 +4,8 @@ def ping_pong(num)
   range.each do |i|
     if i % 3 == 0
       output.push('ping')
+    elsif i % 5 == 0
+      output.push('pong')
     else
       output.push(i)
     end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -9,4 +9,8 @@ describe('#ping_pong') do
   it('replaces every number divisible by 3 and with ping') do
     expect(ping_pong(4)).to(eq([1, 2, 'ping', 4]))
   end
+
+  it('replaces every number divisible by 5 and with pong') do
+    expect(ping_pong(5)).to(eq([1, 2, 'ping', 4, 'pong']))
+  end
 end
